46a226dee4a93d9a7255ad94c00d33e8205e3010
Petre Chitashvili <petre.chitashvili@gmail.com>
2022-01-22 22:00:21 +0400

    Fixed issues with Zoho's project name (AssemblyName) change

--- ZohoCRM.SDK.2.1.Extender.Usage/Program.cs
+++ ZohoCRM.SDK.2.1.Extender.Usage/Program.cs
@@ -4,7 +4,7 @@ using ZohoCRM.Modules.ModuleTypes;
 using ZohoCRM.SDK._2._1.Extender.Usage.Modules;
 using ZohoCRM.SDK._2._1.Tester.Console;
 
-Initialize.SdkInitialize("1000.d7faf845efaf44b944952cbac77355b2.cb6555d7ee822bb3b5f1bfbdac69d303",
+Initialize.SdkInitialize("1000.ba0a37f385079258466fd5b130981f41.c2852432466bb8f650e291462398e02a",
     "1000.VAG312OFJIAV50WN9MQG3ESTSJPC7L",
     "91afdc65d0a7f360479f5433f419cdacaa621e6dbc"
 );
