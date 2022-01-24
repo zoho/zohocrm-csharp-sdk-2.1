bc151dc5cebc1ec9fe6f6605e30afd044a44157c
Petre Chitashvili <petre.chitashvili@gmail.com>
2022-01-24 11:31:47 +0400

    Created ..Extender.BaseTypes project to hold common - safe types

---
+++ ZohoCRM.SDK.2.1.Extender.BaseTypes/OperationTypeNeededInZohoEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace ZohoCRM.SDK_2_1.Extender.BaseTypes;
+
+public enum OperationTypeNeededInZohoEnum
+{
+    Create = 0,
+    Update = 1,
+    LeaveUnchanged = 2,
+    IgnoreDueToError = 3
+}
